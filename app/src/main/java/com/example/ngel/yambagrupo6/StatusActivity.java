30db89fd34ff53a4c262be0d7655f5c237268755
Angelotas <angel.martin.bartolome.95@gmail.com>
2016-11-03 21:35:18 +0100

    Entrega_Interfaz_Fragments

--- app/src/main/java/com/example/ngel/yambagrupo6/StatusActivity.java
+++ app/src/main/java/com/example/ngel/yambagrupo6/StatusActivity.java
@@ -9,7 +9,14 @@ public class StatusActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
+
+        // Comprobar si la actividad ya ha sido creada con anterioridad
+        if (savedInstanceState == null) {
+            // Crear un fragment
+            StatusFragment fragment = new StatusFragment();
+            this.getFragmentManager().beginTransaction()
+                    .add(android.R.id.content, fragment, fragment.getClass().getSimpleName()).commit();
+        }
 
     }
 
